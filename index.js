51bc8b8c59beab400a793ee4354c2cdfeb3135bf
UmangGadhavana <umang.gadhavana@mutualmobile.com>
2023-11-08 14:56:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("thisisug");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
